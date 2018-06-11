462cf493c718c6f429a1509246f7acb38e9b95e9
Andres Puentes <andresf_puentes@hotmail.com>
2018-06-11 22:47:58 +0100

    Section 3, class 22

--- post/code-examples.js
+++ post/code-examples.js
@@ -9,3 +9,42 @@ Direccion:
       Numero: 202
   }
 }
+
+
+//Creation and Hoisting
+
+console.log(a);
+b();
+
+var a = 'Hello World!';
+
+function b(){
+  console.log('function b called!')
+}
+
+
+//Execution Stack
+
+function b(){
+}
+
+function a(){
+  b();
+}
+
+a();
+
+
+//Outer Environments
+
+function b(){
+  console.log(myVar);
+}
+
+function a(){
+  var myVar = 2;
+  b();
+}
+
+var myVar = 1;
+a();
