95b8282baab5645ea30936e3043918f7ab765de4
Andres Puentes <andresf_puentes@hotmail.com>
2018-07-16 18:42:57 +0100

    JS weird parts Booleans Existence

--- post/code-examples.js
+++ post/code-examples.js
@@ -48,3 +48,50 @@ function a(){
 
 var myVar = 1;
 a();
+
+//Synchronous test
+
+function waitThreeSeconds() {
+  var ms = 3000 + new Date().getTime();
+  while (new Date() < ms) {}
+  console.log('finished function')
+}
+
+function clickHandler() {
+  console.log('click event');
+}
+
+//listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
+
+
+//Operator_Precedence
+
+var a = 3 + 4 * 3;
+
+console.log(a);
+
+//Associativity
+
+var a = 2, b = 3, c = 4;
+
+a = b = c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//coercion
+3 > 2 > 1
+true > 1
+1 > 1
+
+//Existence and booleans
+
+var a;
+if(a){
+    console.log('hay algo dentro de a')
+}
